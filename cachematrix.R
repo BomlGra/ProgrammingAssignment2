29fb48bda75ba1e8f8b4feb541618016b09e49ca
Anna <bau614@mail.ru>
2020-05-05 10:22:23 +0300

    Completed task

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## This function  crates cacheable matrix 
 
-makeCacheMatrix <- function(x = matrix()) {
+
+##  * get() - to get internal R matrix
+##  * set(setedInversion) - to set internal R matrix and reset earlier calculated inverse
+##  * getInversion() - to get cached inverse, or NULL if no cached inverse is present
+##  * setInversion(invertedMatrix) - to set cached inverse
+
+makeCacheMatrix <- function(baceMatrix = matrix()) {
+ 
+  invertedMatrix <- NULL
+    set <- function(setedInversion) {
+      baceMatrix <<- setedInversion
+      invertedMatrix <<- NULL
+    }
+    
+    get <- function() baceMatrix
+    setInversion <- function(inversion) invertedMatrix <<- inversion
+    getInversion <- function() invertedMatrix
+    list(set = set, get = get,
+         setInversion = setInversion,
+         getInversion = getInversion)
+  
 
 }
 
 
-## Write a short comment describing this function
+## With operation solve(X) we can return inverse matrix 
+## Function checked if a value was calculated earlier, then caching data and return the matrix or 
+## just return inverted matrix depends on result of this check
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(baceMatrix, ...) {
+
+  invertedMatrix <- baceMatrix$getInversion()
+    if(!is.null(invertedMatrix)) {
+      message("getting cached data")
+      return(invertedMatrix)
+    }
+    data <- baceMatrix$get()
+    invertedMatrix <- solve(data, ...)
+    baceMatrix$setInversion(invertedMatrix)
+    invertedMatrix
+  
+        ## Return a matrix that is the inverse of 'baceMatrix'
 }
